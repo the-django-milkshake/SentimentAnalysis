e06df480247389fe296ac06f3bab105f3d660ecd
Ankit Gupta <argankit@gmail.com>
2019-02-09 19:35:38 +0530

    create django project and virtual environment

---
+++ .env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\ankit\projects\hackathon\the-django-milkshake\.env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
