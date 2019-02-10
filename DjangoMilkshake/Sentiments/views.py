e124d29d84c6a75b7d236a4a4b5764663861d0d8
Ankit Gupta <argankit@gmail.com>
2019-02-10 10:15:07 +0530

    tweets with textblob

--- DjangoMilkshake/Sentiments/views.py
+++ DjangoMilkshake/Sentiments/views.py
@@ -5,6 +5,9 @@ import requests
 import json
 import tweepy
 from tweepy.auth import OAuthHandler
+from textblob import TextBlob
+from langdetect import detect
+
 # Create your views here.
 
 
@@ -17,8 +20,16 @@ def home_timeline(request):
     auth = OAuthHandler('XRCnQ49KVWgy0IsN5QYBTn5Zm', 'P6UwYNbfboKQfrr51P3HLjp88Mq32SxNcQt7zsFKDdAZdXrAoW')
     auth.set_access_token('912853951984238592-BODZqgKvgD0QdKD5Rz1grMGPCDFiZm4', 'proz3qXFAR7Ie8YOylG86z0uERL8orw0HcClAA2X4CN6t')
 
-    api = tweepy.API(auth)
+    api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 
-    public_tweets = api.home_timeline()
+    public_tweets = []
+
+    for tweet in tweepy.Cursor(api.home_timeline).items(5):
+        blob = TextBlob(tweet.text)
+        senti = blob.sentiment.polarity
+        sub = blob.sentiment.subjectivity
+        lang = detect(tweet.text)
+        public_tweets.append({'tweet':tweet, 'subjectivity':sub, 'language':lang})
+
 
     return render(request, 'public_tweets.html', {'public_tweets': public_tweets})
